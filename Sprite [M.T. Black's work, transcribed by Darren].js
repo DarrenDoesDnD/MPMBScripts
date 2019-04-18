9869ee78fad2c3eeb584bd7ecb067a79f37acb5e
Symplistik <darrenlachambre@gmail.com>
2019-04-18 10:02:25 -0400

    Update Sprite [M.T. Black's work, transcribed by Darren].js
    Found another spelling error.

--- Sprite [M.T. Black's work, transcribed by Darren].js
+++ Sprite [M.T. Black's work, transcribed by Darren].js
@@ -47,7 +47,7 @@ RaceList["sprite"] = {
 	weightMetric : " light weight",
 	improvements : "Sprite: +2 Dexterity, +2 Intelligence, -4 Strength;",
 	scores : [-4, 2, 0, 2, 0, 0],
-	trait : "Sprite (+2 Dexterity, +2 Intelligence, -4 Strength)\nPoison Arrows: DC10 CON save or poisoned for 1 minute. If save is 5 or lower, target is unconscious.\nI can case Invisibility once per long rest.\n   At 3rd level, I can cast Detect Thoughts once per long rest as a 2nd-level spell.\nSprite equipment costs 50% more, is 1/5th the weight and weapons inflict 1 die lower damage; 1d4 becomes 1.",
+	trait : "Sprite (+2 Dexterity, +2 Intelligence, -4 Strength)\nPoison Arrows: DC10 CON save or poisoned for 1 minute. If save is 5 or lower, target is unconscious.\nI can cast Invisibility once per long rest.\n   At 3rd level, I can cast Detect Thoughts once per long rest as a 2nd-level spell.\nSprite equipment costs 50% more, is 1/5th the weight, and weapons inflict 1 die lower damage; 1d4 becomes 1.",
 	features : {
 		"Invisibility" : {
 			name : "Invisibility",
