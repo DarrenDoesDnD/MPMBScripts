061f158f2be704ee3c1f2fedbe412cfc489d33d0
Symplistik <darrenlachambre@gmail.com>
2019-04-18 14:20:27 -0400

    Update Sprite [M.T. Black's work, transcribed by Darren].js

--- Sprite [M.T. Black's work, transcribed by Darren].js
+++ Sprite [M.T. Black's work, transcribed by Darren].js
@@ -9,7 +9,7 @@
 /*	-INFORMATION-
 	Subject:	Race
 	Effect:		This script adds a player race, called "Sprite"
-				This subclass has been made by M.T. Black, Greg Marks, & James Introcaso
+				This subclass was been made by M.T. Black, Greg Marks, & James Introcaso
 				This code uses the subclass as it is featured in Mirt's Undermountain Survival Guide
 	Code by:	Darren
 	Date:		2019-04-16 (sheet v13.0.0beta14)
@@ -42,12 +42,12 @@ RaceList["sprite"] = {
     armor : [true, false, false, false],
 	age : " a myserious affair. Some are near immortal, others not.",
 	height : " stand about 18 inches tall.",
-	weight : " light weight.",
+	weight : " weigh around 4 1/2 pounds. (by my own estimation)",
 	heightMetric : " stand around 46cm tall",
-	weightMetric : " light weight",
+	weightMetric : " weigh around 2kg. (by my own estimation)",
 	improvements : "Sprite: +2 Dexterity, +2 Intelligence, -4 Strength;",
 	scores : [-4, 2, 0, 2, 0, 0],
-	trait : "Sprite (+2 Dexterity, +2 Intelligence, -4 Strength)\nPoison Arrows: DC10 CON save or poisoned for 1 minute. If save is 5 or lower, target is unconscious.\nI can cast Invisibility once per long rest.\n   At 3rd level, I can cast Detect Thoughts once per long rest as a 2nd-level spell.\nSprite equipment costs 50% more, is 1/5th the weight, and weapons inflict 1 die lower damage; 1d4 becomes 1.",
+	trait : "Sprite (+2 Dexterity, +2 Intelligence, -4 Strength)\nPoison Arrows: DC10 CON save or poisoned for 1 minute. If save is 5 or lower, target is unconscious for 1 min.\nI can cast Invisibility once per long rest.\n   At 3rd level, I can cast Detect Thoughts once per long rest as a 2nd-level spell.\nSprite equipment costs 50% more, is 1/5th the weight, and weapons inflict 1 die lower damage; 1d4 becomes 1.",
 	features : {
 		"Invisibility" : {
 			name : "Invisibility",
